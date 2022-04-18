cb5e94dd5a710a3391b9acda5024f379d880b6bd
tuan-minh-hust-k64 <tuanminh20194334@gmail.com>
2022-04-18 14:10:04 +0700

    feature 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there.
+          Hi there. i am vu tuan minh
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New Feature
+          Learn React
         </a>
       </header>
     </div>
